86b9fc0dc096c6f1bb98caa4c67501adbc1a42e4
moritsukamasatoshi <yakiyaki_ikayaki@yahoo.co.jp>
2022-03-04 20:51:39 +0900

    一旦コミット

--- spec/tenji_maker_spec.rb
+++ spec/tenji_maker_spec.rb
@@ -1,7 +1,57 @@
 # frozen_string_literal: true
 
 RSpec.describe TenjiMaker do
-  it "does something useful" do
-    expect(false).to eq(false)
+  describe '#to_tenji' do
+    subject do
+      TenjiMaker.new.to_tenji(arg1)
+    end
+
+    context '文字列が渡された場合' do
+      context 'A HI RU が渡された場合' do
+        let!(:arg1) { 'A HI RU' }
+
+        it do
+          expect(subject).to eq <<~TENJI.chomp
+            o- o- oo
+            -- o- -o
+            -- oo --
+          TENJI
+        end
+
+        it { is_expected.to eq <<~TENJI.chomp }
+            o- o- oo
+            -- o- -o
+            -- oo --
+          TENJI
+      end
+
+      context 'KI RI N が渡された場合' do
+        let!(:arg1) { 'KI RI N' }
+
+        it '' do
+          expect(subject).to eq <<~TENJI.chomp
+            o- o- --
+            o- oo -o
+            -o -- oo
+          TENJI
+        end
+      end
+
+      context 'SI MA U MA が渡された場合' do
+        it ''
+      end
+    end
+
+    context '数値が渡された場合' do
+      it ''
+    end
+
+    context 'nil が渡された場合' do
+      it ''
+    end
+
+    context '引数が渡されなかった場合' do
+      it ''
+    end
   end
 end
