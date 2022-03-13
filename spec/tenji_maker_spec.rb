5e9d90b3a9dd87d4e03374fd11e6857eb52488f2
moritsukamasatoshi <yakiyaki_ikayaki@yahoo.co.jp>
2022-03-14 07:25:30 +0900

    引数の数による挙動のテストを追加

--- spec/tenji_maker_spec.rb
+++ spec/tenji_maker_spec.rb
@@ -3,12 +3,12 @@
 RSpec.describe TenjiMaker do
   describe '#to_tenji' do
     subject do
-      TenjiMaker.new.to_tenji(arg1)
+      TenjiMaker.new.to_tenji(*args)
     end
 
     context '文字列が渡された場合' do
       context 'A HI RU が渡された場合' do
-        let!(:arg1) { 'A HI RU' }
+        let!(:args) { ['A HI RU'] }
 
         it do
           expect(subject).to eq <<~TENJI.chomp
@@ -26,7 +26,7 @@ RSpec.describe TenjiMaker do
       end
 
       context 'KI RI N が渡された場合' do
-        let!(:arg1) { 'KI RI N' }
+        let!(:args) { ['KI RI N'] }
 
         it '' do
           expect(subject).to eq <<~TENJI.chomp
@@ -51,7 +51,19 @@ RSpec.describe TenjiMaker do
     end
 
     context '引数が渡されなかった場合' do
-      it ''
+      let!(:args) { [] }
+
+      it '' do
+        expect { subject }.to raise_error ArgumentError
+      end
+    end
+
+    context '引数が2つ渡された場合' do
+      let!(:args) { ['A HI RU', 'KI RI N'] }
+
+      it '' do
+        expect { subject }.to raise_error ArgumentError
+      end
     end
   end
 end
